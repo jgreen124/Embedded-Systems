050d009aacd920e9ae2f89d9f6a0ce1bd2c6360c
jgreen124 <124850546+jgreen124@users.noreply.github.com>
2024-02-08 20:44:33 -0500

    finished part 1 of lab (clock divider)

---
+++ Lab1/clock_div_tb.vhd
@@ -0,0 +1,72 @@
+--
+-- filename:    blinker_tb.vhd
+-- written by:  steve dinicolantonio
+-- description: testbench for blinker.vhd
+-- notes:       
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+entity clock_div_tb is
+end clock_div_tb;
+
+architecture testbench of clock_div_tb is
+
+    signal tb_clk : std_logic := '0';
+    signal tb_en  : std_logic;
+    
+    component clock_div is
+        port(
+        
+            clk  : in std_logic;        -- 125 Mhz clock
+            en   : out std_logic
+
+        );
+    end component;
+
+begin
+
+--------------------------------------------------------------------------------
+-- procs
+--------------------------------------------------------------------------------
+
+    -- simulate a 125 Mhz clock
+    clk_gen_proc: process
+    begin
+    
+        wait for 4 ns;
+        tb_clk <= '1';
+        
+        wait for 4 ns;
+        tb_clk <= '0';
+    
+    end process clk_gen_proc;
+    
+    -- flip the switch high after 1ms
+    --switch_proc: process
+    --begin
+    
+      --  wait for 1 ms;
+      --  tb_sw0 <= '1';
+    
+    --end process switch_proc;
+    
+--------------------------------------------------------------------------------
+-- port mapping
+--------------------------------------------------------------------------------
+
+    dut : clock_div
+    port map (
+    
+        clk  => tb_clk,
+        en => tb_en
+        
+    
+    );
+
+    
+end testbench; 
