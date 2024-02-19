3327f35995c94bb6df80cbcd3cfef02d826a0801
jgreen124 <124850546+jgreen124@users.noreply.github.com>
2024-02-19 01:29:47 -0500

    Add files via upload

--- Lab1/fancy_counter.vhd
+++ Lab1/fancy_counter.vhd
@@ -53,59 +53,36 @@ signal current_dir : std_logic;
 signal current_cnt : std_logic_vector(3 downto 0) := "0000";
 begin
 
+current_val <= val;
 
-clock : process(clk)
+counter_proc : process(clk)
 begin
-    --On the clock rising edge, unless en is 1, nothing will change in the circuit.
---• Even if en is 1, if clk_en is 0 nothing can change the circuit except rst
---• When rst is asserted the cnt value will become 0.
-
---• It can count either up or down depending on the value of a “direction�? register, which is updated
---at the clock rising edge with the value present at dir when updn is 1.
-
---• On the clock rising edge, if ld is 1, the value present at val will be loaded into the “value�? register.
-
---• If counting up, it will count until the number in a 4-bit “value�? register has been reached, at
---which point it will roll over to 0000. If counting down, it will go from 0000 to value when it
---underflows.
-
-    if(rising_edge(clk)) then --clock cycles
-    
-        if (en = '1') then
-            if (rst = '1') then
-                --cnt <= "0000";
-                current_cnt <= "0000"; --reset counter
+if rising_edge(clk) then
+    if en = '1' then
+        current_cnt <= "0000";
+    end if;
+    if (clk_en = '1') then
+        if (updn = '1') then
+            current_dir <= dir;
+        end if;
+        if (current_dir = '1') then
+            if (current_cnt = current_val) then
+                cnt <= "0000";
+            else
+                current_cnt <= std_logic_vector(unsigned(current_cnt)+1);
             end if;
-            if (clk_en = '1') then
-                if (updn = '1') then
-                    current_dir <= dir;
-                end if;
-                if (current_dir = '1') then
-                    if (current_cnt = current_val) then
-                        --cnt <= "0000";
-                        current_cnt <= "0000";
-                    else
-                        --cnt <=std_logic_vector(unsigned(current_cnt) + 1);
-                        current_cnt <=std_logic_vector(unsigned(current_cnt) + 1);
-                    end if;
-                else
-                    if (unsigned(current_cnt) = 0) then
-                        --cnt <=current_val;
-                        current_cnt <= current_val;
-                    else
-                        --cnt <= std_logic_vector(unsigned(current_cnt)-1);
-                        current_cnt <= std_logic_vector(unsigned(current_cnt)-1);
-                    end if;
-                end if;
-                if (ld = '1') then
-                    current_val <= val;
-                end if;
+        else
+            if (unsigned(current_cnt) = 0) then
+                current_cnt <= current_val;
+            else
+                current_cnt <= std_logic_vector(unsigned(current_cnt)-1);
             end if;
         end if;
+        if ld = '1' then
+            current_val <= val;
+        end if;
     end if;
-    cnt <= current_cnt;
-  
-                
+end if;
+cnt <= current_cnt;
 end process;
-
-end Behavioral;
+end architecture;
