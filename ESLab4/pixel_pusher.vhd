1e97abea243ddb7b3caa33fc056995fada62131e
jgreen124 <124850546+jgreen124@users.noreply.github.com>
2024-03-28 02:29:03 -0400

    Add files via upload

--- ESLab4/pixel_pusher.vhd
+++ ESLab4/pixel_pusher.vhd
@@ -32,18 +32,18 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity pixel_pusher is
-    Port ( clk, clk_en, VS : in STD_LOGIC := '0';
-         pixel : in STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
-         hcount : in STD_LOGIC_VECTOR (9 downto 0) := (others => '0');
-         vid : in STD_LOGIC := '0';
-         R, B : out STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
-         G : out STD_LOGIC_VECTOR (5 downto 0) := (others => '0');
+    Port ( clk, clk_en, VS : in STD_LOGIC;
+         pixel : in STD_LOGIC_VECTOR (7 downto 0);
+         hcount : in STD_LOGIC_VECTOR (9 downto 0);
+         vid : in STD_LOGIC;
+         R, B : out STD_LOGIC_VECTOR (4 downto 0);
+         G : out STD_LOGIC_VECTOR (5 downto 0);
          addr : out STD_LOGIC_VECTOR (17 downto 0));
 end pixel_pusher;
 
 architecture pixel_pusher_arch of pixel_pusher is
 
-    signal addr_Reg : std_logic_vector(17 downto 0);
+    signal addr_Reg : std_logic_vector(17 downto 0)  := (others => '0');
 
 begin
 
