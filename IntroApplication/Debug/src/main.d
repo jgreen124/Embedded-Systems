b71484d1758b48a7c51045e2fb257f063277f987
jgreen124 <124850546+jgreen124@users.noreply.github.com>
2024-09-11 18:45:22 -0400

    Added Vitis files from remote connection

---
+++ IntroApplication/Debug/src/main.d
@@ -0,0 +1,33 @@
+src/main.o src/main.o: ../src/main.c \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/bspconfig.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xgpio.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_assert.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xgpio_l.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h \
+ /home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_types.h
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/bspconfig.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xgpio.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_assert.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xgpio_l.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h:
+/home/user/Documents/Embedded-Systems-main/ES2/ES2IntroLab/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_types.h:
